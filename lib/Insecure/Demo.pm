7f0ccb0226a7c990a9e6ac828e03b1b355df36a1
Colin Newell <colin.newell@gmail.com>
2018-11-03 19:47:43 +0000

    Wire in database code

--- lib/Insecure/Demo.pm
+++ lib/Insecure/Demo.pm
@@ -2,6 +2,7 @@ package Insecure::Demo;
 
 use Dancer2;
 use File::ShareDir 'module_dir';
+use Insecure::Demo::Container 'service';
 use Path::Tiny;
 
 # ABSTRACT: demonstration application with security issues
@@ -16,6 +17,23 @@ set template => 'alloy';
 set views    => path( module_dir('Insecure::Demo') )->child('views')->stringify;
 
 our $VERSION = '0.001';
+
+any '/' => sub {
+
+    # prevent naughty people from doing naughty things.
+    if (
+        service('BannedUsers')->is_client_banned(
+            client         => request->user_agent     || '',
+            referer        => request->referer        || '',
+            remote_address => request->remote_address || '',
+            email          => $email,
+        )
+      )
+    {
+        # is this the best way to bounce them?
+        status 400;
+    }
+};
 
 get '/' => sub {
     template 'index' => { 'title' => 'Insecure::Demo' };
